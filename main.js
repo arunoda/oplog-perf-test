87ed2838f8cef34d9833bea15836605a2a62c90f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-07-26 00:06:36 +0530

    Add perf test

--- main.js
+++ main.js
@@ -1,86 +1,55 @@
-items = new Mongo.Collection("items");
-lists = new Mongo.Collection("lists");
+Items = new Mongo.Collection("items");
+var latency = {total: 0, count: 0};
+setInterval(function() {
+	var avgLatency = latency.total/latency.count;
+	console.log("LATENCY:", avgLatency.toFixed(2), "COUNT:", latency.count);
+	latency = {total: 0, count: 0};
+}, 2000);
 
-Router.route("/", function() {
-	this.render("page"); 
-});
-
-Router.route("/:_id", function() {
-	this.render("page", {
-		data: {
-			_id: this.params._id
-		}
-	});
-});
-
-if (Meteor.isClient) {
-	Template.list.onCreated(function() {
-		this.subscribe("items", this.data._id);
-	});
-
-	Template.page.onCreated(function() {
-		if (this.data && this.data._id)
-			this.subscribe("lists", this.data._id);
-	});
-
-	Template.list.helpers({
-		items: function() {
-			return items.find({
-				listId: this._id,
+if(Meteor.isClient) {
+	Start = function() {
+		var totalSubs = 100;
+		function addSub() {
+			Meteor.subscribe('item', function() {
+				totalSubs--;
+				if(totalSubs === 0) {
+					console.log("Adds Added!");
+					setInterval(function() {
+						for(var lc=0; lc<1; lc++) {
+							var start = Date.now();
+							Meteor.call('go', function() {
+								latency.total += Date.now() - start;
+								latency.count++;
+							});
+						}
+					}, 15);
+				} else {
+					addSub();
+				}
 			});
 		}
-	});
 
-	Template.page.helpers({
-		lists: function() {
-			if (!this._id)
-				return;
+		addSub();
 
-			return lists.find({
-				pageId: this._id,
-			});
-		}
-	});
 
-	Template.page.events({
-		"click button": function() {
-			var pageId = Random.id();
-			Meteor.call("createPage", pageId);
-			window.open("/" + pageId);
-		}
-	});
+	};
 }
 
-if (Meteor.isServer) {
-	Meteor.publish("lists", function(pageId) {
-		return lists.find({
-			pageId: pageId,
-		});
-	});
-
-	Meteor.publish("items", function(listId) {
-		return items.find({
-			listId: listId,
-		});
+if(Meteor.isServer) {
+	Meteor.publish('item', function() {
+		var id = Math.ceil(Math.random() * 999999);
+		return Items.find({abc: {$gt: id}});
 	});
 
 	Meteor.methods({
-		"createPage": function(pageId) {
-			for (var i = 0; i < 10; i++) {
-				var listId = Random.id();
-
-				lists.insert({
-					_id: listId,
-					pageId: pageId,
-				});
-
-				for (var k = 0; k < 10; k++) {
-					items.insert({
-						listId: listId,
-						text: "item " + k,
-					});
-				}
-			}
+		go: function() {
+			this.unblock();
+			var start = Date.now();
+			var id = Math.ceil(Math.random() * 1000) + 999999;
+			Items.upsert({_id: "1000"}, {$set: {id: id}});
+			
+			latency.total += Date.now() - start;
+			latency.count++;
 		}
 	});
 }
